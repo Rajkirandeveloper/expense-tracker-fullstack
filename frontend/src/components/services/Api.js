c9cb62b95675423f9d079632348e9231a57477f7
Kelangi Raj Kiran <kelangi.rajkiran@homeshiksha.com>
2024-11-28 14:48:18 +0530

    latest ui changes

--- frontend/src/components/services/Api.js
+++ frontend/src/components/services/Api.js
@@ -1,6 +1,7 @@
 // src/services/api.js
 
 import axios from 'axios';
+import {API_BASE_URL} from "../../config"
 
 const api = axios.create({
     baseURL: 'http://localhost:5000/api',
@@ -8,35 +9,35 @@ const api = axios.create({
 
 // Example: Register User
 export const registerUser = async (data) => {
-    const response = await api.post('http://localhost:3356/api/auth/register', data);
+    const response = await api.post(`${API_BASE_URL}auth/register`, data);
     return response.data;
 };
 
 // Example: Login User
 export const loginUser = async (data) => {
-    const response = await api.post('http://localhost:3356/api/auth/login', data);
+    const response = await api.post(`${API_BASE_URL}auth/login`, data);
     return response.data;
 };
 
 // Add Income
 export const addIncome = async (data) => {
-    const response = await api.post('http://localhost:3356/api/income', data);
+    const response = await api.post(`${API_BASE_URL}income`, data);
     return response.data;
 };
 
 // Add Expense
 export const addExpense = async (data) => {
-    const response = await api.post('http://localhost:3356/api/expense', data);
+    const response = await api.post(`${API_BASE_URL}expense`, data);
     return response.data;
 };
 
 // Get Income and Expenses
 export const getIncome = async () => {
-    const response = await api.get('/income');
+    const response = await api.get(`${API_BASE_URL}income`);
     return response.data;
 };
 
 export const getExpenses = async () => {
-    const response = await api.get('/expense');
+    const response = await api.get(`${API_BASE_URL}expense`);
     return response.data;
 };
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = 'http://localhost:3356/api/'
+// export const API_BASE_URL = 'http://localhost:3356/api/'
+
+export const API_BASE_URL='https://expense-tracker-fullstack-by-raj.onrender.com/api/'
